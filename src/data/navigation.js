6154ae8d23f1c8261443f425c4baa57ee53acadd
jagdeep sidhu <sidhujag@syscoin.org>
2026-04-22 20:10:27 -0700

    Update navigation.js

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -9,7 +9,7 @@ export const NAV_LINKS = [
 export const EXTERNAL_LINKS = {
   docs: 'https://docs.syscoin.org/docs/guides/sentrynode_setup/',
   support: 'https://support.syscoin.org/',
-  github: 'https://github.com/bigpoppa-sys/sysnode-info',
+  github: 'https://github.com/syscoin/sysnode-info',
   discord: 'https://discord.gg/d73qTF9',
   sentryNodeDocs: 'https://docs.syscoin.org/docs/tech/sentrynodes/',
   syscoin: 'https://syscoin.org/',
